2130fcd7976bb0c9f176384a96988b834b8cd9d6
Rafael Zarate <rafazrte@gmail.com>
2018-04-06 12:57:14 -0700

    adding update

--- srcs/main.c
+++ srcs/main.c
@@ -6,12 +6,16 @@
 /*   By: rzarate <rzarate@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/23 17:56:25 by kmckee            #+#    #+#             */
-/*   Updated: 2018/04/06 12:29:39 by rzarate          ###   ########.fr       */
+/*   Updated: 2018/04/06 12:57:03 by rzarate          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lemin.h"
 
+/*
+**	You can access to the linked list of paths (list of movements)
+**	with lemin->paths
+*/
 int	main(void)
 {
 	t_lemin	*lemin;
